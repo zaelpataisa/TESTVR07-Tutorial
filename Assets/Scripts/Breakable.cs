25dad432570128d00358cbe10466f5066c413b16
Zael Aisa <zaelaisa_micronoft@hotmail.com>
2025-09-28 14:10:46 -0400

    Part 4 - Grab and Use

---
+++ Assets/Scripts/Breakable.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable : MonoBehaviour
+{
+    public List<GameObject> breakablePieces;
+    public float timeToBreak = 2;
+    private float timer = 0;
+
+    void Start()
+    {
+        foreach (var item in breakablePieces)
+        {
+            item.SetActive(false);
+        }
+    }
+
+    public void Break()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > timeToBreak)
+        {
+            foreach (var item in breakablePieces)
+            {
+                item.SetActive(true);
+                item.transform.parent = null;
+            }
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/MeteorPistol.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+
+public class MeteorPistol : MonoBehaviour
+{
+    public ParticleSystem particles;
+    public LayerMask layerMask;
+    public Transform shootSource;
+    public float distance = 10;
+
+    private bool rayActivate = false;
+
+    void Start()
+    {
+        XRGrabInteractable grabInteractable = GetComponent<XRGrabInteractable>();
+        grabInteractable.activated.AddListener(x => StartShoot());
+        grabInteractable.deactivated.AddListener(x => StopShoot());
+    }
+
+    public void StartShoot()
+    {
+        particles.Play();
+        rayActivate = true;
+    }
+
+    public void StopShoot()
+    {
+        particles.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        rayActivate = false;
+    }
+
+    void Update()
+    {
+        if (rayActivate)
+        {
+            RaycastCheck();
+        }
+    }
+
+    void RaycastCheck()
+    {
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(shootSource.position, shootSource.forward, out hit, distance, layerMask);
+
+        if (hasHit)
+        {
+            hit.transform.gameObject.SendMessage("Break", SendMessageOptions.DontRequireReceiver);
+        }
+    }
+}
