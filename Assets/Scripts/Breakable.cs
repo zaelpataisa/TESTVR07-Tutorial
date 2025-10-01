6f4e03ca378ac581b92b1b1624df5f0b0b845f50
Zael Aisa <zaelaisa_micronoft@hotmail.com>
2025-10-01 05:42:40 -0400

    Part 9 - User Interface

--- Assets/Scripts/Breakable.cs
+++ Assets/Scripts/Breakable.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Breakable : MonoBehaviour
 {
     public List<GameObject> breakablePieces;
     public float timeToBreak = 2;
     private float timer = 0;
+    public UnityEvent OnBreak;
 
     void Start()
     {
@@ -27,6 +29,7 @@ public class Breakable : MonoBehaviour
                 item.SetActive(true);
                 item.transform.parent = null;
             }
+            OnBreak.Invoke();
             gameObject.SetActive(false);
         }
     }
---
+++ Assets/Scripts/PlaySteps.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.Playables;
+
+public class PlaySteps : MonoBehaviour
+{
+    PlayableDirector director;
+    public List<Step> steps;
+
+    void Start()
+    {
+        director = GetComponent<PlayableDirector>();
+    }
+
+    [System.Serializable]
+    public class Step
+    {
+        public string name;
+        public float time;
+        public bool hasPlayed = false;
+    }
+
+    public void PlayStepIndex(int index)
+    {
+        Step step = steps[index];
+        if (!step.hasPlayed)
+        {
+            step.hasPlayed = true;
+            director.Stop();
+            director.time = step.time;
+            director.Play();
+        }
+    }
+}
